c2fedae7788208780ddbabf7ce48c80f33fc3843
Nguyễn Trung Nhân <112737822+jeremynguyenn@users.noreply.github.com>
2025-01-21 11:11:47 +0700

    wifi project

---
+++ Embedded-STM32-WIFI-Course/1_debug_uart_driver/Debug/Src/esp82xx_driver.d
@@ -0,0 +1,18 @@
+Src/esp82xx_driver.o: ../Src/esp82xx_driver.c ../Inc/esp82xx_driver.h \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Include/core_cm4.h \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Include/cmsis_version.h \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Include/mpu_armv7.h \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../Inc/esp82xx_driver.h:
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Include/core_cm4.h:
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Include/cmsis_version.h:
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Include/cmsis_compiler.h:
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Include/cmsis_gcc.h:
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Include/mpu_armv7.h:
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/1_debug_uart_driver/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
---
+++ Embedded-STM32-WIFI-Course/2_esp_uart_driver/Debug/Src/main.d
@@ -0,0 +1,19 @@
+Src/main.o: ../Src/main.c \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Include/core_cm4.h \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Include/cmsis_version.h \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Include/mpu_armv7.h \
+ D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Inc/esp82xx_driver.h
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Include/core_cm4.h:
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Include/cmsis_version.h:
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Include/cmsis_compiler.h:
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Include/cmsis_gcc.h:
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Include/mpu_armv7.h:
+D:/Embedded-STM32-WIFI-Course/Embedded-STM32-WIFI-Course/2_esp_uart_driver/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Inc/esp82xx_driver.h:
